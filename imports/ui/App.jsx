bc7252cfff03eeee4ee0c7236db82e5bda5ad4fd
freddy <zhonglyu173@gmail.com>
2019-03-16 15:06:49 -0700

    adding db and displaying info from db

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import TopicPost from "./TopicPost.jsx";
 
-
-export default class App extends React.Component {	
+export default class App extends React.Component {
 	render() {
 		return (
 			<div>
 				<h1>Header</h1>
 				<h1>Body</h1>
+				
+				<TopicPost />
+
 				<h1>Footer</h1>
 			</div>
 		);
 	}
 }
-
-
