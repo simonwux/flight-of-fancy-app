5529a841add2c3e00705f5b34c6fb87fc1fa479d
QiminCao <sevenscao@gmail.com>
2019-03-16 21:29:05 -0700

    Added logout on the main page

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { Accounts } from "meteor/accounts-base";
 import TopicPost from "./TopicPost.jsx";
 
 export default class App extends React.Component {
+	onLogout() {
+		Accounts.logout();
+	}
 
 	render() {
 		return (
 			<div>
 				<h1>Header</h1>
+				<button onClick={this.onLogout.bind(this)}>Logout</button>
+				
 				<h1>Body</h1>
 				
 				<TopicPost />
 
+				
 				<h1>Footer</h1>
 			</div>
 		);
