761dbe4fe38a1231b56bd8adfb2568ba53367cf4
QiminCao <sevenscao@gmail.com>
2019-03-16 21:17:20 -0700

    Added sign-up and login

--- imports/ui/Login.jsx
+++ imports/ui/Login.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Meteor } from "meteor/meteor";
 
 
+export default class Login extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: ""
+		};
+	}
 
+	onSubmit(e) {
+		e.preventDefault();
 
-export default class Login extends React.Component {	
+		let email = e.target.email.value.trim();
+		let pwd =  e.target.password.value.trim();
+
+		Meteor.loginWithPassword({email: email}, pwd, (err) => {
+			console.log("Login callback", err);
+		});
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Login here</h1>
+
+				<form onSubmit={this.onSubmit.bind(this)}>
+					<input type="email" name="email" placeholder="Email"/>
+					<input type="password" name="password" placeholder="Password"/>
+					<button>Login</button>
+				</form>
 
 				<Link to="/signup">signup</Link>
 			</div>
--- imports/ui/Signup.jsx
+++ imports/ui/Signup.jsx
@@ -1,13 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { Accounts } from "meteor/accounts-base";
 
 export default class Signup extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: ""
+		};
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+
+		let email = e.target.email.value.trim();
+		let pwd =  e.target.password.value.trim();
+
+		Accounts.createUser({email: email, password: pwd}, (err) => {
+			console.log("Sign up callback", err); 
+		});
+		// this.setState({
+		// 	error: "Something went wrong"
+		// });
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Sign up</h1>
-				
+ 
+				{this.state.error ? <p>{this.state.error}</p> : undefined}
+
+				<form onSubmit={this.onSubmit.bind(this)}>
+					<input type="email" name="email" placeholder="Email"/>
+					<input type="password" name="password" placeholder="Password"/>
+					<button>Create Account</button>
+				</form>
 
 				<Link to="/">login in </Link>
 			</div>
